6775490d424e5356dd02400bc137f981da77b51b
Nayra Guandalini <nayra.guandalini@bradesco.com.br>
2026-09-07 22:30:00 -0300

    Chore: Add class DriverManager

---
+++ src/main/java/io/github/nayliv/driver/DriverManager.java
@@ -0,0 +1,30 @@
+package io.github.nayliv.driver;
+
+import org.openqa.selenium.WebDriver;
+
+public final class DriverManager {
+
+    private static final ThreadLocal<WebDriver> DRIVER =
+            new ThreadLocal<>();
+
+    private DriverManager() {
+    }
+
+    public static WebDriver getDriver() {
+        return DRIVER.get();
+    }
+
+    public static void setDriver(WebDriver driver) {
+        DRIVER.set(driver);
+    }
+
+    public static void quitDriver() {
+
+        WebDriver driver = DRIVER.get();
+
+        if (driver != null) {
+            driver.quit();
+            DRIVER.remove();
+        }
+    }
+}
